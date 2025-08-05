c66d65f5561d17820645269c3dff44937fbbc8f8
inugami-dev64 <karmen@nyaa.ee>
2025-08-05 22:32:34 +0300

    frontend: Assign parameters in map page's +page.svelte

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+    import type { PharmacyInfo } from "$lib/service/pharmacy-info";
+    import type { PageProps } from "../$types";
     import LeafletMap from "../../components/LeafletMap.svelte";
+
+    let { data }: PageProps = $props();
+
+    async function showPharmacyView(pharmacy: PharmacyInfo) {
+        console.log(pharmacy)
+    }
 </script>
 
 <main>
-    <LeafletMap/>
+    <LeafletMap pharmacies={data.pharmacies} callback={showPharmacyView}/>
 </main>
