3d7d8d21613c3a3e328a17ace4c6b63393dc0365
inugami-dev64 <karmen@nyaa.ee>
2025-08-05 22:23:34 +0300

    frontend: Create pharmacy-info.ts  with dummy getPharmacies() implementation

---
+++ frontend/src/lib/service/pharmacy-info.ts
@@ -0,0 +1,28 @@
+
+export class PharmacyInfo {
+    chain: string | undefined;
+    name: string | undefined;
+    aadress: string | undefined;
+    postalCode: number | undefined;
+    city: string | undefined
+    county: string | undefined
+    phoneNumber: string | undefined
+    latitude: number | undefined;
+    longitude: number | undefined;
+}
+
+export async function getPharmacies(): Promise<PharmacyInfo[]> {
+    return [
+        {
+            chain: "Benu",
+            name: "AIA APTEEK",
+            aadress: "Narva mnt 7",
+            postalCode: 10017,
+            city: "Tallinn",
+            county: "Harjumaa",
+            phoneNumber: "+3726109490",
+            latitude: 59.437264,
+            longitude: 24.760051
+        }
+    ]
+}
