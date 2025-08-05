aa6fa1a107b219175be7165eee5041f3dda11ed5
inugami-dev64 <karmen@nyaa.ee>
2025-08-05 19:10:18 +0300

    frontend: Create TransHeart component

---
+++ frontend/src/components/main/TransHeart.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let width;
+    export let height;
+</script>
+
+<span><svg width="{width}" height="{height}" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<path d="M17.3667 3.84172C16.941 3.41589 16.4357 3.0781 15.8795 2.84763C15.3232 2.61716 14.7271 2.49854 14.125 2.49854C13.5229 2.49854 12.9268 2.61716 12.3705 2.84763C11.8143 3.0781 11.309 3.41589 10.8833 3.84172L10 4.72506L9.11666 3.84172C8.25692 2.98198 7.09086 2.49898 5.875 2.49898C4.65914 2.49898 3.49307 2.98198 2.63333 3.84172C1.77359 4.70147 1.29059 5.86753 1.29059 7.08339C1.29059 8.29925 1.77359 9.46531 2.63333 10.3251L10 17.6917L17.3667 10.3251C17.7925 9.89943 18.1303 9.39407 18.3608 8.83785C18.5912 8.28164 18.7099 7.68546 18.7099 7.08339C18.7099 6.48132 18.5912 5.88514 18.3608 5.32893C18.1303 4.77271 17.7925 4.26735 17.3667 3.84172Z" fill="url(#pattern0_3_44)" stroke="#1E1E1E" stroke-linejoin="round"/>
+<defs>
+<pattern id="pattern0_3_44" patternContentUnits="objectBoundingBox" width="1" height="1">
+<use xlink:href="#image0_3_44" transform="matrix(0.05 0 0 0.0573259 0 -0.0732591)"/>
+</pattern>
+<image id="image0_3_44" width="20" height="20" preserveAspectRatio="none" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAABeAAAAnQEYDot0AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAADpJREFUOI1jjD736z8DFQETNQ0bGgYyfl25YzQMKQOM////Hw1DygDLt1U7qWrg4Pcy9dPhaHlIMQAAFXYQFRKcErgAAAAASUVORK5CYII="/>
+</defs>
+</svg>
+</span>
+
+<style>
+    span {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    svg {
+        display: inline-block;
+        vertical-align: middle;
+    }
+</style>
