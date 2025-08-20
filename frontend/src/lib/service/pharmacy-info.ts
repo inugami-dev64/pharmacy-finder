89d18b1baa254b4cc35d2d905560ccc6ffcd3751
inugami-dev64 <karmen@nyaa.ee>
2025-08-20 22:32:09 +0300

    frontend: Restructure PharmacyInfo class and update getPharmacies() function to fetch the API

--- frontend/src/lib/service/pharmacy-info.ts
+++ frontend/src/lib/service/pharmacy-info.ts
@@ -1,36 +1,32 @@
+import type { HttpError } from "$lib/http-error";
 
 export class PharmacyInfo {
+    id: number | undefined;
     chain: string | undefined;
     name: string | undefined;
-    aadress: string | undefined;
-    postalCode: number | undefined;
+    address: string | undefined;
     city: string | undefined
     county: string | undefined
+    postalCode: number | undefined;
     phoneNumber: string | undefined
-    latitude: number | undefined;
-    longitude: number | undefined;
-    overallRating: number | undefined;
-    acceptanceRating: number | undefined;
-    eRating: number | undefined;
-    tRating: number | undefined;
+    lat: number | undefined;
+    lng: number | undefined;
 }
 
 export async function getPharmacies(): Promise<PharmacyInfo[]> {
-    return [
-        {
-            chain: "Benu",
-            name: "AIA APTEEK",
-            aadress: "Narva mnt 7",
-            postalCode: 10017,
-            city: "Tallinn",
-            county: "Harjumaa",
-            phoneNumber: "+3726109490",
-            latitude: 59.437264,
-            longitude: 24.760051,
-            overallRating: 3.8,
-            acceptanceRating: 2.5,
-            eRating: 3.0,
-            tRating: 1
-        }
-    ]
+    return await fetch("/api/v1/pharmacies")
+        .then(async res => {
+            if (res.status != 200) {
+                let err: HttpError = await res.json();
+                console.log(err);
+                throw new Error(`Failed to fetch pharmacies: ${err.msg}`);
+            }
+
+            let data: PharmacyInfo[] = await res.json()
+            return data
+        })
+        .catch(e => {
+            console.log(e);
+            return [];
+        }) /* TODO: Implement a better error signaling system so that the user can see errors */
 }
