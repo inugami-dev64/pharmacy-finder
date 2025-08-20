4dbaa76e4dbdd4691682eef4c1a86a453c03f8df
inugami-dev64 <karmen@nyaa.ee>
2025-08-20 22:03:24 +0300

    backend: Update main.go

--- cmd/pharmafinder/main.go
+++ cmd/pharmafinder/main.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/http"
 	"pharmafinder"
+	v1 "pharmafinder/api/v1"
 	"pharmafinder/db"
 	"time"
 
@@ -14,8 +15,13 @@ import (
 	"go.uber.org/fx"
 )
 
-func NewServerMux() *mux.Router {
+func NewServerMux(routes []Route) *mux.Router {
 	r := mux.NewRouter()
+	apiRouter := r.PathPrefix("/api/v1").Subrouter()
+	for _, route := range routes {
+		apiRouter.Handle(route.Pattern(), route).Methods(route.Methods()...)
+	}
+
 	r.PathPrefix("/").
 		Methods("GET").
 		HandlerFunc(pharmafinder.StaticServer)
@@ -49,13 +55,44 @@ func NewHTTPServer(lc fx.Lifecycle, mux *mux.Router) *http.Server {
 	return server
 }
 
+// Route is an http.Handler that knows the mux pattern
+// under which it will be registered
+type Route interface {
+	http.Handler
+
+	// Pattern reports the relative path at which this is registered
+	Pattern() string
+
+	// Methods reports all HTTP methods that this handler accepts
+	Methods() []string
+}
+
+func AsRoute(f any) any {
+	return fx.Annotate(
+		f,
+		fx.As(new(Route)),
+		fx.ResultTags(`group:"routes"`),
+	)
+}
+
 func main() {
 	// Attempt to load .env files if they exist
 	godotenv.Load("deploy/.env")
 	fx.New(
-		fx.Provide(NewHTTPServer),
-		fx.Provide(NewServerMux),
-		fx.Provide(db.ProvideDatabaseHandle),
+		fx.Provide(
+			NewHTTPServer,
+			fx.Annotate(
+				NewServerMux,
+				fx.ParamTags(`group:"routes"`),
+			),
+
+			// Data access layer
+			db.ProvideDatabaseHandle,
+			db.ProvidePharmacyRepository,
+
+			// /pharmacies/* handlers
+			AsRoute(v1.NewPharmaciesHandler),
+		),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
 }
