2506c8204f0bd058454e7d92e535662d9ff05b45
inugami-dev64 <karmen@nyaa.ee>
2025-08-20 19:51:31 +0300

    backend: Create PharmacyRepository and implement initial methods

---
+++ db/pharmacy_repository.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"pharmafinder/db/entity"
+	"pharmafinder/types"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type PharmacyRepository interface {
+	FindPharmaciesInCoordinateBounds(sw types.Point, ne types.Point) Query[entity.Pharmacy]
+	StoreAll(pharmacies []entity.Pharmacy) error
+	Trx(conn any) PharmacyRepository
+}
+
+type PharmacyRepositorySQLX struct {
+	conn *sqlx.DB
+}
+
+func ProvidePharmacyRepository(conn *sqlx.DB) PharmacyRepository {
+	return PharmacyRepositorySQLX{conn: conn}
+}
+
+func (repo PharmacyRepositorySQLX) FindPharmaciesInCoordinateBounds(sw types.Point, ne types.Point) Query[entity.Pharmacy] {
+	q := `
+	SELECT
+		*
+	FROM
+		pharmacies p
+	WHERE
+		p.latitude >= ?
+	AND
+		p.longitude >= ?
+	AND
+		p.latitude <= ?
+	AND
+		p.longitude <= ?
+	`
+
+	args := []interface{}{sw.Lat, sw.Lng, ne.Lat, ne.Lng}
+
+	return &SQLXQuery[entity.Pharmacy]{
+		uniqueKey: "id",
+		key:       "id",
+		q:         q,
+		args:      args,
+	}
+}
+
+func (repo PharmacyRepositorySQLX) StoreAll(pharmacies []entity.Pharmacy) error {
+	// Separate entities which shall be inserted
+	// and entities which shall be updated
+	_, err := repo.conn.NamedExec(
+		`INSERT INTO pharmacies (chain,"name","address",city,county,postal_code,phone_number,latitude,longitude)
+			VALUES (:chain,:name,:address,:city,:county,:postal_code,:phone_number,:latitude,:longitude)
+		`, pharmacies)
+
+	return err
+}
+
+func (repo PharmacyRepositorySQLX) Trx(conn any) PharmacyRepository {
+	return PharmacyRepositorySQLX{conn: conn.(*sqlx.DB)}
+}
