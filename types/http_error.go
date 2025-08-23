4865170fd108ec6fcd27c982dc1439734e97d5fc
inugami-dev64 <karmen@nyaa.ee>
2025-08-23 15:05:05 +0300

    backend: Create NewHttpError() function

--- types/http_error.go
+++ types/http_error.go
@@ -1,7 +1,17 @@
 package types
 
+import "time"
+
 type HttpError struct {
 	StatusCode int    `json:"code"`
 	Timestamp  Time   `json:"ts"`
 	Message    string `json:"msg"`
+}
+
+func NewHttpError(code int, message string) HttpError {
+	return HttpError{
+		StatusCode: code,
+		Timestamp:  Time(time.Now().UTC()),
+		Message:    message,
+	}
 }
