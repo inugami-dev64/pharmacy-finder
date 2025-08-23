853c5e843d836d0edcb1b5330e506322517b8797
inugami-dev64 <karmen@nyaa.ee>
2025-08-23 15:04:52 +0300

    backend: Rename PharmacyHandler to PharmacyController and change its logic

--- api/v1/pharmacy.go
+++ api/v1/pharmacy.go
@@ -5,96 +5,64 @@ import (
 	"net/http"
 	"pharmafinder/db"
 	"pharmafinder/types"
-	"pharmafinder/utils"
+	"pharmafinder/web"
 	"strconv"
 	"strings"
-	"time"
 )
 
-type PharmaciesHandler struct {
+type PharmaciesController struct {
 	repo db.PharmacyRepository
 }
 
-func NewPharmaciesHandler(repo db.PharmacyRepository) *PharmaciesHandler {
-	return &PharmaciesHandler{repo: repo}
+func ProvidePharmacyController(repo db.PharmacyRepository) []web.Route {
+	controller := &PharmaciesController{repo: repo}
+	return controller.GetRoutes()
 }
 
-func (handler *PharmaciesHandler) Pattern() string {
-	return "/pharmacies"
-}
-
-func (handler *PharmaciesHandler) Methods() []string {
-	return []string{"GET"}
+func (handler *PharmaciesController) GetRoutes() []web.Route {
+	return []web.Route{
+		web.NewRequestsHandler[PharmaciesController](handler.GetPharmacies, "/pharmacies", []string{"GET"}),
+	}
 }
 
 // GET /api/v1/phamacies?sw=lat,lng&ne=lat,lng
-func (handler *PharmaciesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	swText := query.Get("sw")
-	neText := query.Get("ne")
+func (handler *PharmaciesController) GetPharmacies(details *web.HttpRequestDetails) (int, interface{}, error) {
+	swText := details.Params.Get("sw")
+	neText := details.Params.Get("ne")
 
 	swCoords := strings.Split(swText, ",")
 	neCoords := strings.Split(neText, ",")
 
 	if len(swCoords) != 2 || len(neCoords) != 2 {
 		log.Println("Could not extract latitude and longitude from bounds")
-
-		utils.WriteJSONResponse(w, http.StatusBadRequest, types.HttpError{
-			StatusCode: http.StatusBadRequest,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "Missing coordinate bounds",
-		})
-		return
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "Missing coordinate bounds"), nil
 	}
 
 	lat, err := strconv.ParseFloat(swCoords[0], 64)
 	if err != nil {
-		utils.WriteJSONResponse(w, http.StatusBadRequest, types.HttpError{
-			StatusCode: http.StatusBadRequest,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "South-west bound latitude is malformed",
-		})
-		return
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "South-west bound latitude is malformed"), nil
 	}
 	lng, err := strconv.ParseFloat(swCoords[1], 64)
 	if err != nil {
-		utils.WriteJSONResponse(w, http.StatusBadRequest, types.HttpError{
-			StatusCode: http.StatusBadRequest,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "South-west bound longitude is malformed",
-		})
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "South-west bound longitude is malformed"), nil
 	}
 	sw := types.Point{Lat: float32(lat), Lng: float32(lng)}
 
 	lat, err = strconv.ParseFloat(neCoords[0], 64)
 	if err != nil {
-		utils.WriteJSONResponse(w, http.StatusBadRequest, types.HttpError{
-			StatusCode: http.StatusBadRequest,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "North-east bound latitude is malformed",
-		})
-		return
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "North-east bound latitude is malformed"), nil
 	}
 	lng, err = strconv.ParseFloat(neCoords[1], 64)
 	if err != nil {
-		utils.WriteJSONResponse(w, http.StatusBadRequest, types.HttpError{
-			StatusCode: http.StatusBadRequest,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "North-east bound longitude is malformed",
-		})
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "North-east bound longitude is malformed"), nil
 	}
 	ne := types.Point{Lat: float32(lat), Lng: float32(lng)}
 
 	data, err := handler.repo.FindPharmaciesInCoordinateBounds(sw, ne).QueryAll()
 	if err != nil {
 		log.Println("Failed to query pharmacies in coordinate bounds")
-		utils.WriteJSONResponse(w, http.StatusInternalServerError, types.HttpError{
-			StatusCode: http.StatusInternalServerError,
-			Timestamp:  types.Time(time.Now().UTC()),
-			Message:    "Internal server error",
-		})
-		return
+		return http.StatusInternalServerError, nil, err
 	}
 
-	utils.WriteJSONResponse(w, http.StatusOK, data)
+	return http.StatusOK, data, nil
 }
