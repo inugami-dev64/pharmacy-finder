fa5c26352cb41ddb3952f64b52327b6041d5167a
inugami-dev64 <karmen@nyaa.ee>
2025-09-09 23:21:39 +0300

    frontend: Update pharmacy-info.ts so that the request made would be valid

--- frontend/src/lib/service/pharmacy-info.ts
+++ frontend/src/lib/service/pharmacy-info.ts
@@ -8,13 +8,16 @@ export class PharmacyInfo {
     city: string | undefined
     county: string | undefined
     postalCode: number | undefined;
-    phoneNumber: string | undefined
+    phoneNumber: string | undefined;
+    email: string | undefined;
     lat: number | undefined;
     lng: number | undefined;
 }
 
+const ESTONIA_BOUNDS = [[57.74100835592354, 21.73728235397422], [59.694266694887354, 28.30161313994147]]
+
 export async function getPharmacies(): Promise<PharmacyInfo[]> {
-    return await fetch("/api/v1/pharmacies")
+    return await fetch(`/api/v1/pharmacies?sw=${ESTONIA_BOUNDS[0][0]},${ESTONIA_BOUNDS[0][1]}&ne=${ESTONIA_BOUNDS[1][0]},${ESTONIA_BOUNDS[1][1]}`)
         .then(async res => {
             if (res.status != 200) {
                 let err: HttpError = await res.json();
