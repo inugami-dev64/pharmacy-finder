115faa32e8adc709ce0bfcbeeda3e18cfeee0fe6
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 12:36:48 +0300

    backend: Create PharmacyReviewRepository and implement its methods

---
+++ db/pharmacy_review_repository.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"pharmafinder/db/entity"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type PharmacyReviewRepository interface {
+	FindReviewForPharmacy(id int64) Query[entity.PharmacyReview]
+	Store(review *entity.PharmacyReview) error
+	Trx(conn any) PharmacyReviewRepository
+}
+
+type PharmacyReviewRepositorySQLX struct {
+	conn *sqlx.DB
+}
+
+func ProvidePharmacyReviewRepository(conn *sqlx.DB) PharmacyReviewRepository {
+	return PharmacyReviewRepositorySQLX{conn: conn}
+}
+
+func (repo PharmacyReviewRepositorySQLX) FindReviewForPharmacy(id int64) Query[entity.PharmacyReview] {
+	q := `
+	SELECT
+		*
+	FROM
+		pharmacy_reviews pr
+	WHERE
+		pr.pharmacy_id = $1
+	`
+
+	args := []interface{}{id}
+
+	return &SQLXQuery[entity.PharmacyReview]{
+		uniqueKey: "id",
+		key:       "updated_at",
+		trx:       repo.conn,
+		q:         q,
+		args:      args,
+	}
+}
+
+func (repo PharmacyReviewRepositorySQLX) Store(review *entity.PharmacyReview) error {
+	if review.ID != 0 {
+		_, err := repo.conn.NamedExec(
+			`UPDATE pharmacy_reviews SET
+				pharmacy_id = :pharmacy_id,
+				"type" = :type,
+				stars = :stars,
+				hrt_kind = :hrt_kind,
+				nationality = :nationality,
+				review = :review,
+				created_at = :created_at,
+				updated_at = :updated_at
+			WHERE
+				id = :id
+			`, review)
+		return err
+	}
+
+	rows, err := repo.conn.NamedQuery(
+		`INSERT INTO pharmacy_reviews (pharmacy_id,"type",stars,hrt_kind,nationality,review,created_at,updated_at,mod_code)
+			VALUES (:pharmacy_id,:type,:stars,:hrt_kind,:nationality,:review,:created_at,:updated_at,:mod_code)
+		RETURNING *`,
+		review)
+
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		_ = rows.StructScan(review)
+	}
+
+	return nil
+}
+
+func (repo PharmacyReviewRepositorySQLX) Trx(conn any) PharmacyReviewRepository {
+	return PharmacyReviewRepositorySQLX{conn: conn.(*sqlx.DB)}
+}
