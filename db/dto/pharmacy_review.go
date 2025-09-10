7e142663eb702fa42b44dc3a0eb432e86794a0fb
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 17:01:55 +0300

    backend: Add PharmacyReviewsetResultDTO struct

--- db/dto/pharmacy_review.go
+++ db/dto/pharmacy_review.go
@@ -1,9 +1,22 @@
 package dto
 
+import "pharmafinder/types"
+
 type PharmacyReviewCreationDTO struct {
-	PrescriptionType string  `json:"prescriptionType" validate:"required"`
+	PrescriptionType string  `json:"prescriptionType" validate:"required,oneof=Imago GenderGP National"`
 	Stars            int     `json:"stars" validate:"required"`
-	HRTKind          string  `json:"hrtKind" validate:"required"`
+	HRTKind          string  `json:"hrtKind" validate:"required,oneof=t e"`
 	Nationality      *string `json:"nationality" validate:"iso3166_1_alpha2"`
 	Review           *string `json:"review" validate:"lte=1024"`
+}
+
+type PharmacyReviewsetResultDTO struct {
+	ID               int64      `json:"id"`
+	PrescriptionType string     `json:"prescriptionType"`
+	Stars            int        `json:"stars"`
+	HRTKind          string     `json:"hrtKind"`
+	Nationality      *string    `json:"nationality"`
+	Review           *string    `json:"review"`
+	CreatedAt        types.Time `json:"createdAt"`
+	UpdatedAt        types.Time `json:"updatedAt"`
 }
