9d06b6088e71c248cc90ad75f6eb4f635f6c7175
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 13:17:14 +0300

    backend: Create PharmacyReviewCreationDTO struct

---
+++ db/dto/pharmacy_review.go
@@ -0,0 +1,9 @@
+package dto
+
+type PharmacyReviewCreationDTO struct {
+	PrescriptionType string  `json:"prescriptionType" validate:"required"`
+	Stars            int     `json:"stars" validate:"required"`
+	HRTKind          string  `json:"hrtKind" validate:"required"`
+	Nationality      *string `json:"nationality" validate:"iso3166_1_alpha2"`
+	Review           *string `json:"review" validate:"leq=1024"`
+}
