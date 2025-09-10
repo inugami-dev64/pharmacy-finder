4035ed41cee54a84017b17ad8aed3d13d4aed859
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 22:38:34 +0300

    frontend: Create a separate IconButton component with unified style

---
+++ frontend/src/components/common/icons/IconButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let title: string | null = null;
+</script>
+
+<button type="button" on:click aria-label="icon-btn" title={title}>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        border: none;
+        background-color: rgba(0, 0, 0, 0);
+        padding: 0.5em;
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    :global(.svg-btn) {
+        display: inline-block;
+        vertical-align: middle;
+        transition: fill 0.5s ease;
+    }
+
+    :global(.svg-btn:hover) {
+        fill: #10a7ff;
+        cursor: pointer;
+    }
+</style>
