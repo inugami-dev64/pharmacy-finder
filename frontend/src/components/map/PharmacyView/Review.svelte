2e0101bee0f8af1ef0d4fc25fdeeb06a7c01ce18
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 22:40:53 +0300

    frontend: Create an entrypoint for Review component

---
+++ frontend/src/components/map/PharmacyView/Review.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+    import Stars from "../../common/widgets/Stars.svelte";
+    import Estrogen from "$lib/assets/regimen/estrogen.webp";
+    import Testosterone from "$lib/assets/regimen/testosterone.webp";
+    import { mount, onMount } from "svelte";
+    import EditButton from "../../common/icons/EditButton.svelte";
+
+    export let rating: number;
+    export let prescriptionType: string;
+    export let review: string | null;
+    export let regimen: string | null;
+
+    let buttonName: string = "Show more...";
+    let truncateText: boolean = true;
+    let textElement: HTMLElement;
+    let truncatable: boolean = false;
+
+    onMount(async () => {
+        truncatable = textElement.offsetHeight < textElement.scrollHeight || textElement.offsetWidth < textElement.scrollWidth;
+    })
+
+    function toggleReadMore(_: Event) {
+        truncateText = !truncateText;
+        if (truncateText) {
+            buttonName = "Show more..."
+        } else {
+            buttonName = "Show less"
+        }
+    }
+</script>
+
+<div class="phr-review">
+    <div class="review-header">
+        <span>
+            <Stars value={rating} scale={0.75}/>
+            <EditButton size={24}></EditButton>
+        </span>
+        <span>
+        {#if regimen == 'e'}
+        <img src="{Estrogen}" alt="e" title="Estrogen based prescription">
+        {:else if regimen == 't'}
+        <img src="{Testosterone}" alt="t" title="Testosterone based prescription">
+        {/if}
+        </span>
+    </div>
+    <b>{prescriptionType}</b>
+    <div id="content">
+        <p class="{truncateText ? "hide-content" : ""}" bind:this={textElement}>{review}</p>
+    </div>
+
+    {#if truncatable}
+    <button on:click|preventDefault={toggleReadMore} aria-label="Toggle Read More" id="toggleBtn">{buttonName}</button>
+    {/if}
+</div>
+
+<style>
+    .phr-review {
+        margin-right: 15px;
+        padding-bottom: 0.25em;
+        border-bottom: 1px solid #cfcfcf;
+        white-space: pre;
+        text-wrap: wrap;
+    }
+
+    .phr-review > div  {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .review-header {
+        user-select: none;
+    }
+
+    img {
+        width: 24px;
+        height: 24px;
+        user-select: none;
+    }
+
+    .hide-content {
+        overflow: hidden;
+        -webkit-line-clamp: 5;
+        line-clamp: 5;
+        -webkit-box-orient: vertical;
+        box-orient: vertical;
+        display: -webkit-box;
+    }
+</style>
