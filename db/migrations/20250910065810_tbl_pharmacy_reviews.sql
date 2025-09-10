ff058806e2f43d72fa8a08e22ffc586a4e416ed6
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 17:00:11 +0300

    backend: Modify tbl_pharmacy_reviews migration script so that modification_code would support sha256 hex string and add some additional constraints

--- db/migrations/20250910065810_tbl_pharmacy_reviews.sql
+++ db/migrations/20250910065810_tbl_pharmacy_reviews.sql
@@ -6,12 +6,12 @@ CREATE TABLE pharmacy_reviews (
     pharmacy_id BIGINT NOT NULL REFERENCES pharmacies(id),
     prescription_type prescription_t NOT NULL,
     stars INT NOT NULL,
-    hrt_kind BPCHAR(1) NOT NULL,
+    hrt_kind BPCHAR(1) NOT NULL CONSTRAINT hrt_kind_check CHECK (hrt_kind = 'e' OR hrt_kind = 't'),
     nationality BPCHAR(2),
     review TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now(),
-    modification_code VARCHAR(60) NOT NULL
+    modification_code VARCHAR(64) NOT NULL
 );
 
 CREATE INDEX idx_pharmacy_review_pharmacy_id ON pharmacy_reviews (pharmacy_id);
