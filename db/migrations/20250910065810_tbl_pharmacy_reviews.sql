5d61cac88e42d97c9cbadfe002c1c297180f3239
inugami-dev64 <karmen@nyaa.ee>
2025-09-10 10:14:12 +0300

    backend: Create a migration script, which creates pharmacy_reviews table

---
+++ db/migrations/20250910065810_tbl_pharmacy_reviews.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE prescription_t AS ENUM ('Imago', 'GenderGP', 'National');
+CREATE TABLE pharmacy_reviews (
+    id BIGSERIAL PRIMARY KEY,
+    pharmacy_id BIGINT NOT NULL REFERENCES pharmacies(id),
+    prescription_type prescription_t NOT NULL,
+    stars INT NOT NULL,
+    hrt_kind BPCHAR(1) NOT NULL,
+    nationality BPCHAR(2),
+    review TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    modification_code VARCHAR(60) NOT NULL
+);
+
+CREATE INDEX idx_pharmacy_review_pharmacy_id ON pharmacy_reviews (pharmacy_id);
+CREATE INDEX idx_pharmacy_review_updated_at ON pharmacy_reviews (updated_at);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX idx_pharmacy_review_updated_at;
+DROP INDEX idx_pharmacy_review_pharmacy_id;
+DROP TABLE pharmacy_reviews;
+DROP TYPE prescription_t;
+-- +goose StatementEnd
