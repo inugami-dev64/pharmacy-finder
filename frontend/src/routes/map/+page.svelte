53c27c0f690c2da46099015649adf889b8cd5381
inugami-dev64 <karmen@nyaa.ee>
2025-12-23 17:32:01 +0200

    frontend: Update logic in map's +page.svelte so that initial data would be fetched and stored when showing pharmacy view

--- frontend/src/routes/map/+page.svelte
+++ frontend/src/routes/map/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import type { PharmacyInfo } from "$lib/service/pharmacy-info";
+    import { PharmacyRating } from "$lib/service/pharmacy-rating";
+    import { PharmacyReview } from "$lib/service/pharmacy-review";
+    import { ratingData, reviewData } from "$lib/stores";
     import type { PageProps } from "../$types";
     import LeafletMap from "../../components/map/LeafletMap.svelte";
     import PharmacyView from "../../components/map/PharmacyView.svelte";
@@ -10,13 +13,24 @@
     let visible: boolean = $state(false);
 
     async function showPharmacyView(pharmacy: PharmacyInfo) {
+        // empty the reviewData store
+        reviewData.set(undefined);
+        ratingData.set(undefined)
+
         activePharmacy = pharmacy;
         visible = true;
+
+        if (pharmacy.id != null) {
+            reviewData.set(await PharmacyReview.readReviews(pharmacy.id, undefined, undefined));
+            ratingData.set(await PharmacyRating.readPharmacyRatings(pharmacy.id));
+        }
     }
 </script>
 
 <main>
-    <PharmacyView pharmacy={<PharmacyInfo>activePharmacy} bind:visible={visible}/>
+    {#if activePharmacy != null && visible}
+    <PharmacyView pharmacy={<PharmacyInfo>activePharmacy} onClose={() => visible = false}/>
+    {/if}
     <LeafletMap pharmacies={(<{pharmacies: PharmacyInfo[]}>data).pharmacies} callback={showPharmacyView}/>
 </main>
 
