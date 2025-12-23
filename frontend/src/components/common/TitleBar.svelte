41b1f6f56cf0bee839dd1f6d959edd3693e50c9b
inugami-dev64 <karmen@nyaa.ee>
2025-12-23 22:15:59 +0200

    frontend: Create a separate TitleBar component which has the CloseButton

---
+++ frontend/src/components/common/TitleBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import CloseButton from "./icons/CloseButton.svelte";
+
+    export let onClose: () => void;
+</script>
+
+<div class="titlebar">
+    <CloseButton size=32 on:click={(e) => onClose()}/>
+</div>
+
+<style>
+    .titlebar {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: fit-content;
+        justify-content: right;
+    }
+</style>
