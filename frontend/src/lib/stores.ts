f3abec0e6ebb265e4f5a2cf811e95aea33c9023b
inugami-dev64 <karmen@nyaa.ee>
2025-12-23 17:30:11 +0200

    frontend: Add ratingData store to stores.ts

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
 import { PharmacyReview } from "./service/pharmacy-review";
+import type { PharmacyRating } from "./service/pharmacy-rating";
 
-export const reviewData = writable<PharmacyReview[] | undefined>([]);
+export const reviewData = writable<PharmacyReview[] | undefined>(undefined);
+export const ratingData = writable<PharmacyRating[] | undefined>(undefined);
