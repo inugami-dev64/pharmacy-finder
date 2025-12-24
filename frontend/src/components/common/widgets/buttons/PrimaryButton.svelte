51280b0a569ff1c5a93f15c1ff0027574cc08821
inugami-dev64 <karmen@nyaa.ee>
2025-12-24 14:16:28 +0200

    frontend: Create PrimaryButton component

---
+++ frontend/src/components/common/widgets/buttons/PrimaryButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let onclick: ((e: MouseEvent) => void) | undefined = undefined;
+</script>
+
+<button class="primary-btn" onclick={onclick}>
+    <slot></slot>
+</button>
+
+<style>
+    .primary-btn {
+        border: none;
+        border-radius: 0.5em;
+        min-width: 200px;
+        box-shadow: none;
+        background-color: #30bfff;
+        color: white;
+        box-sizing: border-box;
+        padding: 1em;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .primary-btn:hover {
+        cursor: pointer;
+        background-color: #10a7ff;
+    }
+</style>
