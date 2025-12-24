6e5a059ae16c19634daf5112ccfda3c95fbb2bcf
inugami-dev64 <karmen@nyaa.ee>
2025-12-24 15:16:18 +0200

    frontend: Create a new generic ModalWindow component and make use of it in ModifyReviewForm.svelte

---
+++ frontend/src/components/common/ModalWindow.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import TitleBar from "./TitleBar.svelte";
+
+    export let zIndex: number;
+    export let onClose: () => void;
+    export let minWidth: number = 600;
+    export let minHeight: number = 600;
+</script>
+
+<div class="modal-container" style="--zIndex: {zIndex}">
+    <div class="modal-window" style="--minWidth: {minWidth}; --minHeight: {minHeight}">
+        <TitleBar onClose={onClose}/>
+        <slot/>
+    </div>
+</div>
+
+<style>
+    .modal-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: var(--zIndex);
+        width: 100vw;
+        height: 100vh;
+        background-color: rgb(0 0 0 / 50%);
+    }
+
+    .modal-window {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        background-color: #ffffff;
+        width: 50%;
+        height: 50%;
+        border-radius: 1.25em;
+        padding: 1em;
+        border: 1px solid #aaaaaa;
+        min-width: var(--minWidth);
+        min-height: var(--minHeight);
+    }
+</style>
--- frontend/src/components/map/PharmacyView/ModifyReviewForm.svelte
+++ frontend/src/components/map/PharmacyView/ModifyReviewForm.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { PharmacyReview } from "$lib/service/pharmacy-review";
-    import TitleBar from "../../common/TitleBar.svelte";
     import Countries from "$lib/assets/countries.json";
     import type { PharmacyInfo } from "$lib/service/pharmacy-info";
     import Loader from "../../common/widgets/Loader.svelte";
     import PrimaryButton from "../../common/widgets/buttons/PrimaryButton.svelte";
     import StarPicker from "../../common/widgets/stars/StarPicker.svelte";
+    import ModalWindow from "../../common/ModalWindow.svelte";
 
     let {
         pharmacy,
@@ -61,104 +61,74 @@
     }
 </script>
 
-<div class="form-container">
-    <div class="review-form">
-        <TitleBar onClose={onClose}/>
-        <h3>{pharmacy.name}</h3>
-        <form onsubmit={submitForm}>
-            <div class="form-contents">
-                <label for="stars">Your rating*:</label>
-                <StarPicker name="stars" defaultChecked={review?.stars ?? 5}/>
-                <label for="review-comment">Comment:</label>
-                <textarea
-                    name="review-comment"
-                    placeholder="Enter your comment here"
-                >{review?.review}</textarea>
-                <div>
-                    <label for="nationality">Nationality*:</label>
-                    <select name="nationality" required>
-                        {#each Object.keys(Countries).sort() as key}
-                            {#if review == null && key === "EE" || review != null && key === review.nationality}
-                            <option value={key} selected>{`${Countries[key as keyof typeof Countries].emoji} ${Countries[key as keyof typeof Countries].name}`}</option>
-                            {:else}
-                            <option value={key}>{`${Countries[key as keyof typeof Countries].emoji} ${Countries[key as keyof typeof Countries].name}`}</option>
-                            {/if}
-                        {/each}
-                    </select>
-                </div>
-                <div>
-                    <label for="hrt-kind">HRT kind*:</label>
-                    <select name="hrt-kind" required>
-                        <option value="e" selected={review != null && review.hrtKind === 'e'}>Estrogen</option>
-                        <option value="t" selected={review != null && review.hrtKind === 't'}>Testosterone</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="prescription-type">Prescription type*:</label>
-                    <select name="prescription-type" required>
-                        <option value="Imago" selected={review != null && review.prescriptionType === "Imago"}>Imago</option>
-                        <option value="GenderGP" selected={review != null && review.prescriptionType === "GenderGP"}>GenderGP</option>
-                        <option value="National" selected={review != null && review.prescriptionType === "National"}>National</option>
-                    </select>
-                </div>
-
-                {#if review != null}
-                    <div>
-                        <label for="mod-code">Modification code*:</label>
-                        <input type="text" name="mod-code" required>
-                    </div>
-                {/if}
+<ModalWindow zIndex={10000} onClose={onClose}>
+    <h3>{pharmacy.name}</h3>
+    <form onsubmit={submitForm}>
+        <div class="form-contents">
+            <label for="stars">Your rating*:</label>
+            <StarPicker name="stars" defaultChecked={review?.stars ?? 5}/>
+            <label for="review-comment">Comment:</label>
+            <textarea
+                name="review-comment"
+                placeholder="Enter your comment here"
+            >{review?.review}</textarea>
+            <div>
+                <label for="nationality">Nationality*:</label>
+                <select name="nationality" required>
+                    {#each Object.keys(Countries).sort() as key}
+                        {#if review == null && key === "EE" || review != null && key === review.nationality}
+                        <option value={key} selected>{`${Countries[key as keyof typeof Countries].emoji} ${Countries[key as keyof typeof Countries].name}`}</option>
+                        {:else}
+                        <option value={key}>{`${Countries[key as keyof typeof Countries].emoji} ${Countries[key as keyof typeof Countries].name}`}</option>
+                        {/if}
+                    {/each}
+                </select>
+            </div>
+            <div>
+                <label for="hrt-kind">HRT kind*:</label>
+                <select name="hrt-kind" required>
+                    <option value="e" selected={review != null && review.hrtKind === 'e'}>Estrogen</option>
+                    <option value="t" selected={review != null && review.hrtKind === 't'}>Testosterone</option>
+                </select>
+            </div>
+            <div>
+                <label for="prescription-type">Prescription type*:</label>
+                <select name="prescription-type" required>
+                    <option value="Imago" selected={review != null && review.prescriptionType === "Imago"}>Imago</option>
+                    <option value="GenderGP" selected={review != null && review.prescriptionType === "GenderGP"}>GenderGP</option>
+                    <option value="National" selected={review != null && review.prescriptionType === "National"}>National</option>
+                </select>
             </div>
 
-            {#if pendingSubmission}
-            <div style="display: flex; justify-content: center; width: 100%">
-                <Loader/>
-            </div>
-            {:else if invalidModCode}
-            <p style="color: red">Invalid modification code</p>
-            {:else if newReview.modCode}
-            <p>Your modification code is: <span style="color: green">{newReview.modCode}</span></p>
-            {:else}
-            <PrimaryButton>
-                {review == null ? "Create a review" : "Modify review"}
-            </PrimaryButton>
+            {#if review != null}
+                <div>
+                    <label for="mod-code">Modification code*:</label>
+                    <input type="text" name="mod-code" required>
+                </div>
             {/if}
-        </form>
-    </div>
-</div>
+        </div>
+
+        {#if pendingSubmission}
+        <div style="display: flex; justify-content: center; width: 100%">
+            <Loader/>
+        </div>
+        {:else if invalidModCode}
+        <p style="color: red">Invalid modification code</p>
+        {:else if newReview.modCode}
+        <p>Your modification code is: <span style="color: green">{newReview.modCode}</span></p>
+        {:else}
+        <PrimaryButton>
+            {review == null ? "Create a review" : "Modify review"}
+        </PrimaryButton>
+        {/if}
+    </form>
+</ModalWindow>
 
 
 <style>
     h3 {
         padding: 0;
         margin-bottom: 0.5em;
-    }
-
-    .form-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        left: 0;
-        top: 0;
-        z-index: 10000;
-        width: 100vw;
-        height: 100vh;
-        background-color: rgb(0 0 0 / 50%);
-    }
-
-    .review-form {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        background-color: #ffffff;
-        width: 50%;
-        height: 50%;
-        border-radius: 1.25em;
-        padding: 1em;
-        border: 1px solid #aaaaaa;
-        min-width: 600px;
-        min-height: 600px;
     }
 
     form {
