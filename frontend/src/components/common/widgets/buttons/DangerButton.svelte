742ab3926198fe805313123929d3a4845aecc3eb
inugami-dev64 <karmen@nyaa.ee>
2025-12-24 15:49:42 +0200

    frontend: Create DangerButton component

---
+++ frontend/src/components/common/widgets/buttons/DangerButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let onclick: ((e: MouseEvent) => void) | undefined = undefined;
+</script>
+
+<button class="danger-btn" onclick={onclick}>
+    <slot/>
+</button>
+
+<style>
+    .danger-btn {
+        border: none;
+        border-radius: 0.5em;
+        min-width: 200px;
+        box-shadow: none;
+        background-color: #dc3545;
+        color: white;
+        box-sizing: border-box;
+        padding: 1em;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .danger-btn:hover {
+        cursor: pointer;
+        background-color: #bb2d3b;
+    }
+</style>
