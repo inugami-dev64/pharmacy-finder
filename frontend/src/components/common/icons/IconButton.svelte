24abdbedd6f14a98d393fa9228f14827759fdd31
inugami-dev64 <karmen@nyaa.ee>
2025-12-28 12:37:22 +0200

    frontend: Add href prop to IconButton component, so that some buttons would get rendered as hyperlinks

--- frontend/src/components/common/icons/IconButton.svelte
+++ frontend/src/components/common/icons/IconButton.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
     export let title: string | null = null;
     export let hoverColor: string = "#10a7ff";
+    export let href: string | undefined = undefined;
 </script>
 
+{#if href == null}
 <button style="--hover-color: {hoverColor}" type="button" on:click aria-label="icon-btn" title={title}>
     <slot></slot>
 </button>
+{:else}
+<a href="{href}" target="_blank" style="--hover-color: {hoverColor}" type="button" on:click aria-label="icon-btn" title={title}>
+    <slot></slot>
+</a>
+{/if}
 
 <style>
-    button {
+    button, a {
         border: none;
         background-color: rgba(0, 0, 0, 0);
         padding: 0.5em;
--- frontend/src/components/common/icons/SourceCodeButton.svelte
+++ frontend/src/components/common/icons/SourceCodeButton.svelte
@@ -4,9 +4,10 @@
 
     export let size: number;
     export let title: string = "View source code";
+    export let href: string = "https://github.com/inugami-dev64/pharmacy-finder"
 </script>
 
-<IconButton on:click title="{title}">
+<IconButton on:click title="{title}" href={href}>
     <svg class="svg-btn" xmlns="http://www.w3.org/2000/svg" height="{size}" viewBox="0 -960 960 960" width="{size}" fill="#999999">
         <path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/>
     </svg>
