e0b8594e7c848acbe6b137a8f32437a1f5b70aca
inugami-dev64 <karmen@nyaa.ee>
2025-12-28 12:31:03 +0200

    frontend: Create a NavBar component

---
+++ frontend/src/components/common/widgets/navbar/NavBar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let size: number = 64;
+</script>
+
+<div class="navbar" style="--size: {size}px">
+    <slot/>
+</div>
+
+<style>
+    .navbar {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        min-width: var(--size);
+        min-height: var(--size);
+        background-color: #ffffff;
+        border: 1px solid #aaaaaa;
+        border-radius: 0.5em;
+        box-sizing: border-box;
+    }
+</style>
