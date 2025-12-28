5466e97960d63595f18c21ea3e63cea12e9a3424
inugami-dev64 <karmen@nyaa.ee>
2025-12-28 12:29:23 +0200

    frontend: Add new IconButton based components

---
+++ frontend/src/components/common/icons/SourceCodeButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { number } from "svelte-i18n";
+    import IconButton from "./IconButton.svelte";
+
+    export let size: number;
+    export let title: string = "View source code";
+</script>
+
+<IconButton on:click title="{title}">
+    <svg class="svg-btn" xmlns="http://www.w3.org/2000/svg" height="{size}" viewBox="0 -960 960 960" width="{size}" fill="#999999">
+        <path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z"/>
+    </svg>
+</IconButton>
