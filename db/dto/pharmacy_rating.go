be436d5d47bad96a5c7de40184c8c2ee39c53897
inugami-dev64 <karmen@nyaa.ee>
2026-01-01 16:06:54 +0200

    backend: Create a DTO struct for tier list rating data

--- db/dto/pharmacy_rating.go
+++ db/dto/pharmacy_rating.go
@@ -5,3 +5,11 @@ type PharmacyRatingDTO struct {
 	Stars   float32 `db:"stars" json:"stars"`
 	HRTKind *string `db:"hrt_kind" json:"hrtKind"`
 }
+
+type PharmacyTierRatingDTO struct {
+	ID         int64   `db:"id" json:"id"`
+	Name       string  `db:"name" json:"name"`
+	AvgRating  float64 `db:"avg_rating" json:"avgRating"`
+	AvgERating float64 `db:"avg_e_rating" json:"avgERating"`
+	AvgTRating float64 `db:"avg_t_rating" json:"avgTRating"`
+}
