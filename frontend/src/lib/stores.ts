7c2c00a792313e451f70676cc1d5f95589aec69c
inugami-dev64 <karmen@nyaa.ee>
2026-01-01 17:26:19 +0200

    frontend: Add PharmacyTierRating as a store

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,6 +1,7 @@
 import { writable } from "svelte/store";
 import { PharmacyReview } from "./service/pharmacy-review";
-import type { PharmacyRating } from "./service/pharmacy-rating";
+import { PharmacyTierRating, type PharmacyRating } from "./service/pharmacy-rating";
 
 export const reviewData = writable<PharmacyReview[] | undefined>(undefined);
 export const ratingData = writable<PharmacyRating[] | undefined>(undefined);
+export const tierRatingData = writable<PharmacyTierRating[] | undefined>(undefined);
