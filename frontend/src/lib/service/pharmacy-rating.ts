257f61a7e1aa90f13b86142459f8d615ec99325d
inugami-dev64 <karmen@nyaa.ee>
2026-01-01 16:53:11 +0200

    frontend: Add PharmacyTierRating class to pharmacy-rating.ts

--- frontend/src/lib/service/pharmacy-rating.ts
+++ frontend/src/lib/service/pharmacy-rating.ts
@@ -1,4 +1,5 @@
 import type { HttpError } from "$lib/http-error";
+import { Point } from "$lib/utils/point";
 
 export class PharmacyRating {
     id: number | undefined;
@@ -22,6 +23,40 @@ export class PharmacyRating {
 
                 let data: PharmacyRating[] = await res.json()
                 return data
+            })
+            .catch(e => {
+                console.log(e);
+                return [];
+            })
+    }
+}
+
+/**
+ * Tier ratings are mainly used in the tier-list
+ */
+export class PharmacyTierRating {
+    id: number | undefined;
+    name: string | undefined;
+    avgRating: number | undefined;
+    avgERating: number | undefined;
+    avgTRating: number | undefined;
+
+    public static async readPharmacyTierRatings(sw?: Point, ne?: Point): Promise<PharmacyTierRating[]> {
+        if (sw == null)
+            sw = new Point(-90, -90);
+        if (ne == null)
+            ne = new Point(90, 90);
+
+        return await fetch(`/api/v1/pharmacies/ratings?sw=${sw.lat},${sw.lng}&ne=${ne.lat},${ne.lng}`)
+            .then(async res => {
+                if (res.status != 200) {
+                    let err: HttpError = await res.json();
+                    console.log(err);
+                    throw new Error(`Failed to fetch pharmacy tier ratings`);
+                }
+
+                let data: PharmacyTierRating[] = await res.json();
+                return data;
             })
             .catch(e => {
                 console.log(e);
