19fa26f0016985aa4405c6189bd9d810aa337028
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 10:55:57 +0200

    backend: Create comment_reviews SQL table creation migration script

---
+++ db/migrations/20260131084236_tbl_comment_reviews.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE comment_review_result_t AS ENUM ('APPROVED', 'PERSONAL_ATTACK', 'OFFENSIVE', 'OTHER');
+CREATE TABLE comment_reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    comment_id BIGINT NOT NULL REFERENCES pharmacy_reviews(id),
+    moderator_id UUID NOT NULL REFERENCES moderator_users(id),
+    result comment_review_result_t NOT NULL,
+    mod_comment VARCHAR(1024) NULL,
+    marked_for_deletion BOOLEAN NOT NULL DEFAULT FALSE,
+    reviewed_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_comment_reviews_marked_for_deletion_reviewed_at ON comment_reviews(marked_for_deletion, reviewed_at);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX idx_comment_reviews_marked_for_deletion_reviewed_at;
+DROP TABLE comment_reviews;
+DROP TYPE comment_review_result_t;
+-- +goose StatementEnd
