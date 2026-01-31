a3c3996b4ce43f52c7054539bd76e2a9c6e404c8
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 20:56:40 +0200

    backend: Put token data into a separate structure from AuthenticatedModeratorUserResponseDTO

--- db/dto/moderator_user.go
+++ db/dto/moderator_user.go
@@ -2,19 +2,21 @@ package dto
 
 import "pharmafinder/types"
 
+type SessionTokenResponseDTO struct {
+	Token    string `db:"-" json:"token"`
+	ValidFor int64  `db:"-" json:"validFor"`
+}
+
 type AuthenticatedModeratorUserResponseDTO struct {
-	ID                    types.UUID `db:"id" json:"id"`
-	Username              string     `db:"username" json:"username"`
-	Email                 string     `db:"email" json:"email"`
-	FirstName             string     `db:"first_name" json:"firstName"`
-	LastName              string     `db:"last_name" json:"lastName"`
-	RegistrationTimestamp types.Time `db:"registration_timestamp" json:"registrationTs"`
-	LastLoginTimestamp    types.Time `db:"last_login_timestamp" json:"lastLoginTs"`
-	Administrator         bool       `db:"administrator" json:"administrator"`
-	Session               struct {
-		Token    string `db:"-" json:"token"`
-		ValidFor int64  `db:"-" json:"validFor"`
-	} `db:"-" json:"session"`
+	ID                    types.UUID              `db:"id" json:"id"`
+	Username              string                  `db:"username" json:"username"`
+	Email                 string                  `db:"email" json:"email"`
+	FirstName             string                  `db:"first_name" json:"firstName"`
+	LastName              string                  `db:"last_name" json:"lastName"`
+	RegistrationTimestamp types.Time              `db:"registration_timestamp" json:"registrationTs"`
+	LastLoginTimestamp    types.Time              `db:"last_login_timestamp" json:"lastLoginTs"`
+	Administrator         bool                    `db:"administrator" json:"administrator"`
+	Session               SessionTokenResponseDTO `db:"-" json:"session"`
 }
 
 type ModeratorUserListResponseDTO struct {
