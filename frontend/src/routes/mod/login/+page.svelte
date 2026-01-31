258e5d8dbbf278edff51d504c6d413c770a227d9
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 10:23:35 +0200

    frontend: Add language switcher to mod/login/+page.svelte

--- frontend/src/routes/mod/login/+page.svelte
+++ frontend/src/routes/mod/login/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { _ } from "svelte-i18n";
-    import LoginForm from "../../../components/common/widgets/LoginForm.svelte";
-    import Recaptcha from "../../../components/common/Recaptcha.svelte";
+    import UserForm from "../../../components/common/widgets/UserForm.svelte";
+    import LanguageSwitcher from "../../../components/common/LanguageSwitcher.svelte";
 
     let clientSideValidationErrors = $state(["Please complete the recaptcha challenge"])
 </script>
@@ -11,18 +11,27 @@
 </svelte:head>
 
 <main>
-    <LoginForm
-        title="Login to moderation panel"
-        submitBtnText="Login"
+    <div class="lang-container">
+        <LanguageSwitcher/>
+    </div>
+    <UserForm
+        title={$_("mod.login.formTitle")}
+        submitBtnText={$_("mod.login.submitTitle")}
     >
-        <input type="text" name="username" placeholder="Username" required maxlength="32">
-        <input type="password" name="password" placeholder="Password" required>
-    </LoginForm>
+        <input type="text" name="username" placeholder={$_("mod.login.usernamePlaceholder")} required maxlength="32">
+        <input type="password" name="password" placeholder={$_("mod.login.passwordPlaceholder")} required>
+    </UserForm>
 </main>
 
 <style>
     :global(body) {
         background-color: #fdcece;
+    }
+
+    .lang-container {
+        position: absolute;
+        top: 0;
+        left: 0;
     }
 
     main {
