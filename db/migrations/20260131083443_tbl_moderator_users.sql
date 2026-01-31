891009196455a1301806074f9f1222aceeeb7ee9
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 15:29:02 +0200

    backend: Add email, registration_timestamp and last_login_timestamp values to moderator_users SQL table

--- db/migrations/20260131083443_tbl_moderator_users.sql
+++ db/migrations/20260131083443_tbl_moderator_users.sql
@@ -1,19 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE moderator_users (
-    id UUID NOT NULL PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username VARCHAR(32) NOT NULL,
+    email VARCHAR(64) NOT NULL,
     "password" VARCHAR(60) NOT NULL,
     first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
+    registration_timestamp TIMESTAMP DEFAULT now() NOT NULL,
+    last_login_timestamp TIMESTAMP DEFAULT now() NOT NULL,
     administrator BOOLEAN NOT NULL DEFAULT FALSE
 );
-CREATE INDEX idx_moderator_users_username ON moderator_users (username);
+CREATE UNIQUE INDEX idx_moderator_users_username ON moderator_users (username);
+CREATE UNIQUE INDEX idx_moderator_users_email ON moderator_users (email);
 CREATE INDEX idx_moderator_users_first_name_last_name ON moderator_users (first_name, last_name);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX idx_moderator_users_username;
 DROP INDEX idx_moderator_users_first_name_last_name;
+DROP INDEX idx_moderator_users_email;
+DROP INDEX idx_moderator_users_username;
+DROP TABLE moderator_users;
 -- +goose StatementEnd
