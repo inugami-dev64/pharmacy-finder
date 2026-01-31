ac415569fa39e86ed0869df3e49e02e20edb6cd4
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 22:24:26 +0200

    backend: Create some additional moderator user related DTO structures

--- db/dto/moderator_user.go
+++ db/dto/moderator_user.go
@@ -36,6 +36,18 @@ type ModeratorUserRegistrationDTO struct {
 	LastName  string `json:"lastName" validate:"required,lte=64"`
 }
 
+type AdminUserUpdateDTO struct {
+	Email     string  `json:"email" validate:"required,email,lte=64"`
+	FirstName string  `json:"firstName" validate:"required,lte=64"`
+	LastName  string  `json:"lastName" validate:"required,lte=64"`
+	Password  *string `json:"password" validate:"lte=72"`
+}
+
+type ModeratorUserUpdateDTO struct {
+	AdminUserUpdateDTO
+	CurrentPassword string `json:"currentPassword" validate:"required,lte=72"`
+}
+
 type ModeratorUserLoginDTO struct {
 	Username string `json:"username" validate:"required,lte=32"`
 	Password string `json:"password" validate:"required,lte=72"`
