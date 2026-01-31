41c2f255c2d0613eee6cf09b7daa2a53a2ce7d72
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 21:43:46 +0200

    backend: Create a controller method for /mod/auth/register/admin

--- api/v1/mod/auth/auth.go
+++ api/v1/mod/auth/auth.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 	"pharmafinder/db"
 	"pharmafinder/db/dto"
+	"pharmafinder/db/entity"
 	"pharmafinder/service"
 	"pharmafinder/types"
 	"pharmafinder/utils"
 	"pharmafinder/web"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/rs/zerolog"
 )
 
@@ -54,8 +56,6 @@ func (handler *ModeratorAuthController) GetRoutes() []web.Route {
 // @Param			request body dto.ModeratorUserLoginDTO true "Login request body"
 // @Success 		200 {object} dto.AuthenticatedModeratorUserResponseDTO
 // @Failure			400 {object} types.HttpError
-// @Failure			403 {object} types.HttpError
-// @Failure			404 {object} types.HttpError
 // @Router			/api/v1/mod/auth/login [post]
 func (handler *ModeratorAuthController) ModeratorLogin(details *web.HttpRequestDetails[dto.ModeratorUserLoginDTO]) (int, interface{}, error) {
 	user, err := handler.repo.FindUserByUsernameOrEmail(details.Body.Username).Query()
@@ -64,11 +64,11 @@ func (handler *ModeratorAuthController) ModeratorLogin(details *web.HttpRequestD
 	}
 
 	if user == nil {
-		return http.StatusNotFound, types.NewHttpError(http.StatusNotFound, "User with provided username or email was not found"), nil
+		return http.StatusBadRequest, types.NewHttpError(http.StatusNotFound, "User with provided username or email was not found"), nil
 	}
 
 	if !handler.hasher.Verify(details.Body.Password, user.Password) {
-		return http.StatusForbidden, types.NewHttpError(http.StatusForbidden, "Invalid password"), nil
+		return http.StatusBadRequest, types.NewHttpError(http.StatusForbidden, "Invalid password"), nil
 	}
 
 	user.LastLoginTimestamp = types.Time(time.Now().UTC())
@@ -92,8 +92,63 @@ func (handler *ModeratorAuthController) ModeratorLogin(details *web.HttpRequestD
 	return http.StatusOK, userDTO, nil
 }
 
+// First time admin user registration endpoint
+//
+// Path /api/v1/mod/auth/register/admin
+//
+// @Summary 		Register the initial admin account
+// @Description		Endpoint which can be used to register the initial administrator account when admin accounts are not present
+// @Tags			Auth
+// @Produce			json
+// @Param			request body dto.ModeratorUserRegistrationDTO true "Registration request body"
+// @Success 		200 {object} dto.AuthenticatedModeratorUserResponseDTO
+// @Failure			400 {object} types.HttpError
+// @Failure			403 {object} types.HttpError
+// @Router			/api/v1/mod/auth/register/admin [post]
 func (handler *ModeratorAuthController) AdminRegistration(details *web.HttpRequestDetails[dto.ModeratorUserRegistrationDTO]) (int, interface{}, error) {
-	return http.StatusOK, []string{}, nil
+	pwdHash, err := handler.hasher.CreatePasswordHash(details.Body.Password)
+	if err == service.ErrPasswordTooLong {
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "Password is too long"), nil
+	} else if err != nil {
+		return http.StatusInternalServerError, nil, err
+	}
+
+	user := entity.ModeratorUser{
+		Username:              details.Body.Username,
+		Email:                 details.Body.Email,
+		Password:              pwdHash,
+		FirstName:             details.Body.FirstName,
+		LastName:              details.Body.LastName,
+		RegistrationTimestamp: types.Time(time.Now().UTC()),
+		LastLoginTimestamp:    types.Time(time.Now().UTC()),
+		Administrator:         true,
+	}
+
+	err = handler.repo.Store(&user)
+	if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+		handler.logger.Warn().Msgf("User with username '%s' and email '%s' already exists", details.Body.Username, details.Body.Email)
+		return http.StatusBadRequest, types.NewHttpError(http.StatusBadRequest, "Username or email is already in use"), nil
+	} else if err != nil {
+		handler.logger.Warn().Msgf("Failed to register an admin account: %v", err)
+		return http.StatusInternalServerError, nil, err
+	}
+
+	userDTO := dto.AuthenticatedModeratorUserResponseDTO{
+		ID:                    user.ID,
+		Username:              user.Username,
+		Email:                 user.Email,
+		FirstName:             user.FirstName,
+		LastName:              user.LastName,
+		RegistrationTimestamp: user.RegistrationTimestamp,
+		LastLoginTimestamp:    user.LastLoginTimestamp,
+		Administrator:         user.Administrator,
+		Session: dto.SessionTokenResponseDTO{
+			Token:    handler.sessionManager.NewSessionToken(&user),
+			ValidFor: int64(service.SESSION_TTL.Seconds()),
+		},
+	}
+
+	return http.StatusOK, userDTO, nil
 }
 
 func (handler *ModeratorAuthController) ModeratorRegistration(details *web.HttpRequestDetails[dto.ModeratorUserRegistrationDTO]) (int, interface{}, error) {
