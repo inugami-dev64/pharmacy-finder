74dd6b48e9ae6551822951c1e4d3bbe9dacb24f1
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 10:14:33 +0200

    frontend: Create a moderation panel login page

---
+++ frontend/src/routes/mod/login/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import favicon from "$lib/assets/favicon.ico";
+
+    let { children } = $props();
+
+    import '$lib/i18n';
+</script>
+
+<svelte:head>
+    <meta
+        name="description"
+        content="Login to Moderation panel - Pharmacy Finder">
+    <meta
+        property="og:description"
+        content="Login to Moderation panel - Pharmacy Finder">
+    <meta
+        property="og:title"
+        content="Pharmacy Finder">
+    <meta
+        property="og:image"
+        content={favicon}>
+    <link rel="icon" href={favicon}/>
+</svelte:head>
+
+{@render children?.()}
---
+++ frontend/src/routes/mod/login/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+    import LoginForm from "../../../components/common/widgets/LoginForm.svelte";
+    import Recaptcha from "../../../components/common/Recaptcha.svelte";
+
+    let clientSideValidationErrors = $state(["Please complete the recaptcha challenge"])
+</script>
+
+<svelte:head>
+    <title>Login - Pharmacy Finder</title>
+</svelte:head>
+
+<main>
+    <LoginForm
+        title="Login to moderation panel"
+        submitBtnText="Login"
+    >
+        <input type="text" name="username" placeholder="Username" required maxlength="32">
+        <input type="password" name="password" placeholder="Password" required>
+    </LoginForm>
+</main>
+
+<style>
+    :global(body) {
+        background-color: #fdcece;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
