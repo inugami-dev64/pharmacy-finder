a0fc077be0a2f66ef24e83737a1108ec45f2f29f
inugami-dev64 <karmen@nyaa.ee>
2026-01-31 15:29:23 +0200

    backend: Update ModeratorUser entity struct

--- db/entity/moderator_user.go
+++ db/entity/moderator_user.go
@@ -3,10 +3,13 @@ package entity
 import "pharmafinder/types"
 
 type ModeratorUser struct {
-	ID            types.UUID `db:"id"`
-	Username      string     `db:"username"`
-	Password      string     `db:"password"`
-	FirstName     string     `db:"first_name"`
-	LastName      string     `db:"last_name"`
-	Administrator bool       `db:"administrator"`
+	ID                    types.UUID `db:"id"`
+	Username              string     `db:"username"`
+	Email                 string     `db:"email"`
+	Password              string     `db:"password"`
+	FirstName             string     `db:"first_name"`
+	LastName              string     `db:"last_name"`
+	RegistrationTimestamp types.Time `db:"registration_timestamp"`
+	LastLoginTimestamp    types.Time `db:"last_login_timestamp"`
+	Administrator         bool       `db:"administrator"`
 }
