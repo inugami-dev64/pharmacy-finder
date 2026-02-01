36c1f091aa83ea07749257482447a9e7916612c8
inugami-dev64 <karmen@nyaa.ee>
2026-02-01 13:54:04 +0200

    backend: Add additional constraints to comment_reviews SQL table

--- db/migrations/20260131084236_tbl_comment_reviews.sql
+++ db/migrations/20260131084236_tbl_comment_reviews.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TYPE comment_review_result_t AS ENUM ('APPROVED', 'PERSONAL_ATTACK', 'OFFENSIVE', 'OTHER');
+CREATE TYPE comment_review_result_t AS ENUM ('APPROVED', 'PERSONAL_ATTACK', 'OFFENSIVE', 'OTHER', 'NONE');
 CREATE TABLE comment_reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     comment_id BIGINT NOT NULL REFERENCES pharmacy_reviews(id),
@@ -8,14 +8,18 @@ CREATE TABLE comment_reviews (
     result comment_review_result_t NOT NULL,
     mod_comment VARCHAR(1024) NULL,
     marked_for_deletion BOOLEAN NOT NULL DEFAULT FALSE,
-    reviewed_at TIMESTAMP NOT NULL DEFAULT now()
+    reviewed_at TIMESTAMP NOT NULL DEFAULT now(),
+    CHECK (result = 'APPROVED' AND NOT marked_for_deletion OR result != 'APPROVED'),
+    CHECK (result != 'NONE')
 );
 
 CREATE INDEX idx_comment_reviews_marked_for_deletion_reviewed_at ON comment_reviews(marked_for_deletion, reviewed_at);
+CREATE UNIQUE INDEX idx_comment_reviews_comment_id_moderator_id ON comment_reviews(comment_id, moderator_id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX idx_comment_reviews_comment_id_moderator_id;
 DROP INDEX idx_comment_reviews_marked_for_deletion_reviewed_at;
 DROP TABLE comment_reviews;
 DROP TYPE comment_review_result_t;
