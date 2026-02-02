af517dfdac0de45620e421877e84a67bb8b8868b
inugami-dev64 <karmen@nyaa.ee>
2026-02-02 17:48:14 +0200

    frontend: Create AccountButton component

---
+++ frontend/src/components/common/icons/buttons/AccountButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import AccountIcon from "../AccountIcon.svelte";
+    import IconButton from "./IconButton.svelte";
+
+    export let size: number;
+    export let title: string;
+</script>
+
+<IconButton on:click title="{title}">
+    <AccountIcon size={size} globalClass="svg-btn"/>
+</IconButton>
