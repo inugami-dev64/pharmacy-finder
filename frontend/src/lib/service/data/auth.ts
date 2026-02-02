8acdf4c1848aafbc744071294ef19c79d6ce529d
inugami-dev64 <karmen@nyaa.ee>
2026-02-02 14:33:20 +0200

    frontend: Create relevant classes for authenticated relation procedures alongside with methods to make fetch requests

---
+++ frontend/src/lib/service/data/auth.ts
@@ -0,0 +1,96 @@
+import { HttpError } from "$lib/http-error";
+import { LocalizedBackendError } from "./error";
+import type { FetchFunc } from "./fetch";
+
+/**
+ * AuthenticatedUser class represents the successful authentication
+ * response from the server
+ */
+export class AuthenticatedUser {
+    id?: string;
+    username?: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    registrationTs?: number;
+    lastLoginTs?: number;
+    administrator?: boolean;
+    session?: {
+        token: string;
+        validFor?: number;
+    }
+}
+
+export class LoginForm {
+    username?: string;
+    password?: string;
+
+    /**
+     * Login to moderation platform using credentials in the LoginForm instance
+     *
+     * @param fetch specifies the fetch function to use for making a request
+     * @returns a Promise to AuthenticatedUser
+     */
+    public async login(fetch: FetchFunc): Promise<AuthenticatedUser> {
+        return await fetch(`/api/v1/mod/auth/login`, {
+            method: "POST",
+            body: JSON.stringify(this),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(async res => {
+                if (res.status != 200) {
+                    let err: HttpError = await res.json();
+                    console.error(err);
+                    if (err.msg?.startsWith("User with provided"))
+                        throw new LocalizedBackendError("mod.login.errors.userNotFound");
+                    else if (err.msg?.startsWith("Invalid password"))
+                        throw new LocalizedBackendError("mod.login.errors.invalidPassword");
+                    else throw new LocalizedBackendError("universalErrors.internalServerError");
+                }
+
+                let data: AuthenticatedUser = await res.json();
+                return data;
+            })
+    }
+}
+
+export class RegistrationForm {
+    username: string | undefined;
+    email: string | undefined;
+    password: string | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+
+    /**
+     * Register a new administrator account using credentials in the
+     * RegistrationForm instance and authenticate with it
+     *
+     * @param fetch specifies the fetch function to use for making requests
+     * @returns a Promise to AuthenticatedUser object
+     */
+    public async registerAdministrator(fetch: FetchFunc): Promise<AuthenticatedUser> {
+        return await fetch(`/api/v1/mod/auth/register`, {
+            method: "POST",
+            body: JSON.stringify(this),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(async res => {
+                if (res.status != 200) {
+                    let err: HttpError = await res.json();
+                    console.error(err);
+                    if (err.msg?.startsWith("Username or email"))
+                        throw new LocalizedBackendError("mod.register.errors.usernameOrEmailExists");
+                    else if (err.msg?.startsWith("Password is too long"))
+                        throw new LocalizedBackendError("mod.register.errors.passwordTooLong");
+                    else throw new LocalizedBackendError("universalErrors.internalServerError");
+                }
+
+                let data: AuthenticatedUser = await res.json();
+                return data;
+            })
+    }
+}
