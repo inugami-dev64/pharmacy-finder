579d77276186708df25630be90edf66b348009fe
inugami-dev64 <karmen@nyaa.ee>
2026-02-02 14:34:56 +0200

    frontend: Write admin user registration form submission logic to /mod/register/+page.svelte

--- frontend/src/routes/mod/register/+page.svelte
+++ frontend/src/routes/mod/register/+page.svelte
@@ -2,12 +2,56 @@
     import { _ } from "svelte-i18n";
     import UserForm from "../../../components/common/widgets/UserForm.svelte";
     import LanguageSwitcher from "../../../components/common/LanguageSwitcher.svelte";
+    import { RegistrationForm } from "$lib/service/data/auth";
+    import { LocalizedBackendError } from "$lib/service/data/error";
+    import { authenticationSession } from "$lib/service/auth-session";
+    import { goto } from "$app/navigation";
 
-    let clientSideValidationErrors = $state(["Please complete the recaptcha challenge"])
+    let clientSideValidationErrors: Array<string> = $state([]);
+    let isSubmitted: boolean = $state(false);
+
+    async function submitForm(e: SubmitEvent) {
+        e.preventDefault();
+        clientSideValidationErrors = [];
+        isSubmitted = true
+        const form = e.target as HTMLFormElement;
+        const data = new FormData(form);
+
+        const username = data.get("username");
+        const email = data.get("email");
+        const password = data.get("password");
+        const confirmPassword = data.get("confirmPassword");
+        const firstName = data.get("firstName");
+        const lastName = data.get("lastName");
+
+        if (password != confirmPassword) {
+            clientSideValidationErrors.push("mod.register.errors.passwordsDoNotMatch");
+            isSubmitted = false;
+            return
+        }
+
+        let regForm = new RegistrationForm();
+        regForm.username = username?.toString(),
+        regForm.email = email?.toString(),
+        regForm.password = password?.toString(),
+        regForm.firstName = firstName?.toString(),
+        regForm.lastName = lastName?.toString()
+
+        try {
+            const adminUser = await regForm.registerAdministrator(fetch);
+            authenticationSession.setSessionToken(adminUser.session?.token || "", adminUser.session?.validFor || 0);
+            goto("/mod", { replaceState: true });
+        } catch (e) {
+            if (e instanceof LocalizedBackendError)
+                clientSideValidationErrors.push(e.msg);
+        }
+
+        isSubmitted = false;
+    }
 </script>
 
 <svelte:head>
-    <title>Login - Pharmacy Finder</title>
+    <title>Register - Pharmacy Finder</title>
 </svelte:head>
 
 <main>
@@ -17,6 +61,9 @@
     <UserForm
         title={$_("mod.register.formTitle")}
         submitBtnText={$_("mod.register.submitTitle")}
+        isSubmitted={isSubmitted}
+        validationErrors={clientSideValidationErrors}
+        submitForm={submitForm}
     >
         <input type="text" name="username" placeholder={$_("mod.register.usernamePlaceholder")} required maxlength="32">
         <input type="email" name="email" placeholder={$_("mod.register.emailPlaceholder")} required maxlength="64">
