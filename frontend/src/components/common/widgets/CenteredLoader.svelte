8096a397c79f826f415c2a1b6ed9afc801f7dc91
inugami-dev64 <karmen@nyaa.ee>
2026-02-04 11:49:36 +0200

    frontend: Create a CenteredLoader component

---
+++ frontend/src/components/common/widgets/CenteredLoader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Loader from "./Loader.svelte";
+    export let node: HTMLElement | undefined = undefined;
+</script>
+
+<div class="loader-container" bind:this={node}>
+    <Loader/>
+</div>
+
+<style>
+    .loader-container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin-top: 1em;
+        height: fit-content;
+    }
+</style>
