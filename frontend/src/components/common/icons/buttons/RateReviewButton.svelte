b2168cf1d41f3213dd8bdbf60001278d0156c921
inugami-dev64 <karmen@nyaa.ee>
2026-02-07 12:58:03 +0200

    frontend: Create RateReviewIcon and RateReviewButton components

---
+++ frontend/src/components/common/icons/buttons/RateReviewButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import RateReviewIcon from "../RateReviewIcon.svelte";
+    import IconButton from "./IconButton.svelte";
+
+    export let size: number;
+    export let title: string = "Rate this review";
+</script>
+
+<IconButton on:click title={title}>
+    <RateReviewIcon size={size} globalClass="svg-btn"/>
+</IconButton>
