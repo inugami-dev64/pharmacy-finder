71afe212af42bed24328edebc5abee0f0e572e59
inugami-dev64 <karmen@nyaa.ee>
2026-02-07 13:29:29 +0200

    frontend: Create new icon components

---
+++ frontend/src/components/common/icons/IntermediateCheckIcon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let globalClass: string | undefined = undefined;
+    export let color: string | undefined = "#BEA804";
+    export let size: number;
+</script>
+
+<svg class="{globalClass}" xmlns="http://www.w3.org/2000/svg" height="{size}" viewBox="0 -960 960 960" width="{size}" fill="{color}">
+    <path d="M240-440v-80h480v80H240Z"/>
+</svg>
