ac15a5db45882ceb4f70c18f71e25189cdc53a52
inugami-dev64 <karmen@nyaa.ee>
2026-02-10 13:52:01 +0200

    frontend: Add onclick prop to AccountButton component

--- frontend/src/components/common/icons/buttons/AccountButton.svelte
+++ frontend/src/components/common/icons/buttons/AccountButton.svelte
@@ -6,8 +6,9 @@
     export let title: string;
     export let color: string | undefined = "#999999"
     export let padding: number | undefined = 0.5;
+    export let onclick: (() => void) | undefined = undefined;
 </script>
 
-<IconButton on:click title="{title}" padding={padding}>
+<IconButton on:click={() => onclick?.()} title="{title}" padding={padding}>
     <AccountIcon size={size} globalClass="svg-btn" color={color}/>
 </IconButton>
