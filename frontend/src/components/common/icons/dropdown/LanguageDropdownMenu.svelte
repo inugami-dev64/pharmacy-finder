db5d9bccbea232f95bdc613b29b24c64dabb6a42
inugami-dev64 <karmen@nyaa.ee>
2026-02-10 13:57:02 +0200

    frontend: Create a separate LanguageDropdownMenu component

---
+++ frontend/src/components/common/icons/dropdown/LanguageDropdownMenu.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { localeSwitcher } from "$lib/service/locale";
+    import LanguageButton from "../buttons/LanguageButton.svelte";
+    import DropdownMenuContainer from "./DropdownMenuContainer.svelte";
+
+    export let size: number;
+    export let title: string | undefined = "Change language";
+    export let color: string | undefined = "#999";
+</script>
+
+<DropdownMenuContainer
+    size={size}
+    ButtonComponent={LanguageButton}
+    title={title}
+    color={color}
+>
+    <button onclick={() => {
+        localeSwitcher.setLocale("en");
+    }}>
+        <span style="font-size: 32px;">🇬🇧</span>
+        <p>English</p>
+    </button>
+    <button onclick={() => {
+        localeSwitcher.setLocale("et");
+    }}>
+        <span style="font-size: 32px;">🇪🇪</span>
+        <p>eesti</p>
+    </button>
+</DropdownMenuContainer>
