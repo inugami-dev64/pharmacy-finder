eb8e660f335da8e6278a1f6087ac78c8a96582bb
inugami-dev64 <karmen@nyaa.ee>
2026-02-15 19:00:16 +0200

    frontend: Implement a load function for /mod/accounts/[slug] endpoint, which fetches the user account

--- frontend/src/routes/mod/accounts/[slug]/+page.ts
+++ frontend/src/routes/mod/accounts/[slug]/+page.ts
@@ -1,7 +1,41 @@
-export function load({ params }) {
+import { authenticationSession } from '$lib/service/auth-session';
+import { LocalizedBackendError } from '$lib/service/data/error.js';
+import { HealthCheckResult } from '$lib/service/data/health';
+import { UserProfile } from '$lib/service/data/users.js';
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ params }) {
+    let profile: UserProfile | undefined = undefined;
+    let error: LocalizedBackendError | undefined = undefined;
+
+    // Check if the page should be redirected
+    const health = await HealthCheckResult.readHealthCheck(fetch);
+    if (!health?.initialized)
+        redirect(307, "/mod/register");
+    else if (authenticationSession.getSessionToken() == null)
+        redirect(307, "/mod/login");
+
+    // If the slug is "me", then query data about the current user
     const userId: string = params.slug;
-    
+    if (userId === "me") {
+        try {
+            profile = await UserProfile.getAuthenticatedUser();
+        } catch (e) {
+            if (e instanceof LocalizedBackendError)
+                error = e;
+        }
+    } else {
+        try {
+            profile = await UserProfile.getUserById(userId);
+        } catch (e) {
+            if (e instanceof LocalizedBackendError)
+                error = e;
+        }
+    }
+
     return {
-        slug: params.slug
+        profile: profile,
+        error: error,
+        isMe: userId === "me"
     }
 }
