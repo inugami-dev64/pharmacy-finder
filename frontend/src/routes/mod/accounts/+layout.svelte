10d043770b1070e64ed6abe08d46cff8b8ad8845
inugami-dev64 <karmen@nyaa.ee>
2026-02-15 17:39:00 +0200

    frontend: Create an entrypoint  for /mod/accounts

---
+++ frontend/src/routes/mod/accounts/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import favicon from "$lib/assets/favicon.ico";
+
+    let { children } = $props();
+
+    import '$lib/i18n';
+</script>
+
+<svelte:head>
+    <meta
+        name="description"
+        content="Moderator users - Pharmacy Finder">
+    <meta
+        property="og:description"
+        content="Moderator users - Pharmacy Finder">
+    <meta
+        property="og:title"
+        content="Pharmacy Finder">
+    <meta
+        property="og:image"
+        content={favicon}>
+    <link rel="icon" href={favicon}/>
+</svelte:head>
+
+{@render children?.()}
