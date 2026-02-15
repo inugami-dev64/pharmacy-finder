2c2ca9cebe0b5836e68abce1659582f0644352c3
inugami-dev64 <karmen@nyaa.ee>
2026-02-15 18:45:00 +0200

    frontend: Make use of localizations in LanguageDropdownMenu component

--- frontend/src/components/common/icons/dropdown/LanguageDropdownMenu.svelte
+++ frontend/src/components/common/icons/dropdown/LanguageDropdownMenu.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import { _ } from "svelte-i18n";
     import { localeSwitcher } from "$lib/service/locale";
     import LanguageButton from "../buttons/LanguageButton.svelte";
     import DropdownMenuContainer from "./DropdownMenuContainer.svelte";
 
     export let size: number;
-    export let title: string | undefined = "Change language";
+    export let title: string | undefined = $_("languageSwitcher.changeLanguage");
     export let color: string | undefined = "#999";
 </script>
 
