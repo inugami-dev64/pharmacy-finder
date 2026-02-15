d425656dec23080cd7f152ef2dfac2d18593a747
inugami-dev64 <karmen@nyaa.ee>
2026-02-15 18:37:56 +0200

    frontend: Create ShieldIcon and ShieldButton components

---
+++ frontend/src/components/common/icons/buttons/ShieldButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import ShieldIcon from "../ShieldIcon.svelte";
+    import IconButton from "./IconButton.svelte";
+
+    export let size: number;
+    export let title: string;
+    export let color: string | undefined = "#999";
+    export let padding: number | undefined = 0.5;
+    export let onclick: (() => void) | undefined = undefined;
+</script>
+
+<IconButton on:click={() => onclick?.()} title="{title}" padding={padding}>
+    <ShieldIcon size={size} globalClass="svg-btn" color={color}/>
+</IconButton>
