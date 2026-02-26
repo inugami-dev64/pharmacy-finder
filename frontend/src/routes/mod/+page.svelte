4b041f1b03f2ff87cd85dbcd6cbd794fd50ca5ee
inugami-dev64 <karmen@nyaa.ee>
2026-02-26 18:21:26 +0200

    frontend: Make use of ModerationListContainer component in mod/+page.svelte

--- frontend/src/routes/mod/+page.svelte
+++ frontend/src/routes/mod/+page.svelte
@@ -13,10 +13,11 @@
     import IntermediateCheckIcon from "../../components/common/icons/IntermediateCheckIcon.svelte";
     import BellNotificationIcon from "../../components/common/icons/BellNotificationIcon.svelte";
     import ModerationModal from "../../components/mod/ModerationModal.svelte";
-    import { ReviewModerationResult } from "$lib/service/data/moderation";
+    import { Moderation, ReviewModerationResult } from "$lib/service/data/moderation";
     import { onMount } from "svelte";
     import { localeSwitcher } from "$lib/service/locale";
     import ModeratorHeader from "../../components/mod/ModeratorHeader.svelte";
+    import ModerationListContainer from "../../components/mod/ModerationListContainer.svelte";
 
     class ReviewFilter {
         key?: number;
@@ -68,96 +69,94 @@
     isAdmin={authenticationSession.isAdmin(authenticationSession.getSessionToken() || "")}
 />
 
-<div class="mod-container">
-    <div class="comment-container">
-        <TitleBar>
-            <div class="filters">
-                <label for="orderBy">{$_("mod.filters.orderBy.title")}:</label>
-                <select
-                    name="orderBy"
-                    bind:value={orderByValue}
-                    onchange={() => {
-                        console.log(orderByValue)
-                        if (orderByValue == "newest")
-                            filter.desc = true;
-                        else filter.desc = false;
-                        clearPager();
-                    }}
-                >
-                    <option selected={filter.desc} value="newest">{$_("mod.filters.orderBy.newestFirst")}</option>
-                    <option selected={!filter.desc} value="oldest">{$_("mod.filters.orderBy.oldestFirst")}</option>
-                </select>
-                <label for="unmoderated">{$_("mod.filters.showUnmoderated")}:</label>
-                <input
-                    type="checkbox"
-                    name="unmoderated"
-                    bind:checked={filter.showUnmoderated}
-                    onchange={(_) => clearPager()}
-                >
-                <label for="moderated">{$_("mod.filters.showModerated")}:</label>
-                <input
-                    type="checkbox"
-                    name="moderated"
-                    bind:checked={filter.showModerated}
-                    onchange={(_) => clearPager()}
-                >
-            </div>
-        </TitleBar>
-        {#each reviews as review}
-            <Review review={review}>
-                {#if !review.markedForDeletion && review.commentReviewResult !== ReviewModerationResult.None}
-                    <span title="{$_("mod.comments.approved")}" class="icon">
-                        <CheckCircleIcon size={24}/>
-                    </span>
-                {:else if review.markedForDeletion}
-                    <span title="{$_("mod.comments.markedForDeletion")}" class="icon">
-                        <TimedDeletionIcon size={24}/>
-                    </span>
-                {:else if review.commentReviewResult === ReviewModerationResult.None}
-                    <span title="{$_("mod.comments.notModerated")}" class="icon">
-                        <IntermediateCheckIcon size={24}/>
-                    </span>
-                {/if}
-                {#if review.commentReviewResult === ReviewModerationResult.PersonalAttack}
-                    <span title="{$_("mod.comments.personalAttack")}" class="icon">
-                        <BellNotificationIcon size={24}/>
-                    </span>
-                {:else if review.commentReviewResult === ReviewModerationResult.Offensive}
-                    <span title="{$_("mod.comments.offensive")}" class="icon">
-                        <BellNotificationIcon size={24}/>
-                    </span>
-                {:else if review.commentReviewResult === ReviewModerationResult.Other}
-                    <span title="{$_("mod.comments.other")}" class="icon">
-                        <BellNotificationIcon size={24}/>
-                    </span>
-                {/if}
-                <RateReviewButton
-                    size={24}
-                    title="Rate the review"
-                    on:click={() => {
-                        pendingReview = review;
-                        showModerationModal = true;
-                    }}
-                />
-            </Review>
-        {/each}
-        {#if reviews.length === 0 && fetchDone}
-            <div style="width: 100%; text-align: center; margin-top: 1em">
-                <i>{$_("mod.noReviews")}</i>
-            </div>
-        {/if}
-        {#if !fetchDone}
-            <IntersectionObserver
-                {element}
-                on:intersect={(e: CustomEvent<IntersectionObserverEntry>) => {
-                    updateReviewList().then();
+<ModerationListContainer>
+    <TitleBar>
+        <div class="filters">
+            <label for="orderBy">{$_("mod.filters.orderBy.title")}:</label>
+            <select
+                name="orderBy"
+                bind:value={orderByValue}
+                onchange={() => {
+                    console.log(orderByValue)
+                    if (orderByValue == "newest")
+                        filter.desc = true;
+                    else filter.desc = false;
+                    clearPager();
                 }}
             >
-                <CenteredLoader bind:node={element}/>
-            </IntersectionObserver>
-        {/if}
-    </div>
-</div>
+                <option selected={filter.desc} value="newest">{$_("mod.filters.orderBy.newestFirst")}</option>
+                <option selected={!filter.desc} value="oldest">{$_("mod.filters.orderBy.oldestFirst")}</option>
+            </select>
+            <label for="unmoderated">{$_("mod.filters.showUnmoderated")}:</label>
+            <input
+                type="checkbox"
+                name="unmoderated"
+                bind:checked={filter.showUnmoderated}
+                onchange={(_) => clearPager()}
+            >
+            <label for="moderated">{$_("mod.filters.showModerated")}:</label>
+            <input
+                type="checkbox"
+                name="moderated"
+                bind:checked={filter.showModerated}
+                onchange={(_) => clearPager()}
+            >
+        </div>
+    </TitleBar>
+    {#each reviews as review}
+        <Review review={review}>
+            {#if !review.markedForDeletion && review.commentReviewResult !== ReviewModerationResult.None}
+                <span title="{$_("mod.comments.approved")}" class="icon">
+                    <CheckCircleIcon size={24}/>
+                </span>
+            {:else if review.markedForDeletion}
+                <span title="{$_("mod.comments.markedForDeletion")}" class="icon">
+                    <TimedDeletionIcon size={24}/>
+                </span>
+            {:else if review.commentReviewResult === ReviewModerationResult.None}
+                <span title="{$_("mod.comments.notModerated")}" class="icon">
+                    <IntermediateCheckIcon size={24}/>
+                </span>
+            {/if}
+            {#if review.commentReviewResult === ReviewModerationResult.PersonalAttack}
+                <span title="{$_("mod.comments.personalAttack")}" class="icon">
+                    <BellNotificationIcon size={24}/>
+                </span>
+            {:else if review.commentReviewResult === ReviewModerationResult.Offensive}
+                <span title="{$_("mod.comments.offensive")}" class="icon">
+                    <BellNotificationIcon size={24}/>
+                </span>
+            {:else if review.commentReviewResult === ReviewModerationResult.Other}
+                <span title="{$_("mod.comments.other")}" class="icon">
+                    <BellNotificationIcon size={24}/>
+                </span>
+            {/if}
+            <RateReviewButton
+                size={24}
+                title="Rate the review"
+                on:click={() => {
+                    pendingReview = review;
+                    showModerationModal = true;
+                }}
+            />
+        </Review>
+    {/each}
+    {#if reviews.length === 0 && fetchDone}
+        <div style="width: 100%; text-align: center; margin-top: 1em">
+            <i>{$_("mod.noReviews")}</i>
+        </div>
+    {/if}
+    {#if !fetchDone}
+        <IntersectionObserver
+            {element}
+            on:intersect={(e: CustomEvent<IntersectionObserverEntry>) => {
+                updateReviewList().then();
+            }}
+        >
+            <CenteredLoader bind:node={element}/>
+        </IntersectionObserver>
+    {/if}
+</ModerationListContainer>
 
 {#if showModerationModal && pendingReview != null}
     <ModerationModal
@@ -177,24 +176,5 @@
     .icon {
         display: inline-block;
         vertical-align: middle;
-    }
-
-    .mod-container {
-        width: 100%;
-        height: calc(100% - 65px);
-        overflow: hidden;
-        box-sizing: border-box;
-    }
-
-    .comment-container {
-        margin: 30px auto;
-        overflow: auto;
-        max-height: calc(100% - 60px);
-        max-width: 50%;
-        min-width: 400px;
-        box-sizing: border-box;
-        padding: 0.5em;
-        border-radius: 0.5em;
-        background-color: #ffffff;
     }
 </style>
